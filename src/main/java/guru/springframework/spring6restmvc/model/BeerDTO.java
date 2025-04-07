256d4e6c032625b60b188459992bd7d65da33ea8
Amonizz <andrea.monizza@gmail.com>
2025-04-07 18:55:22 +0200

    adding flyway dependency

--- src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -26,6 +28,7 @@ public class BeerDTO {
     private String beerName;
 
     @NotNull
+    @JdbcTypeCode(value = SqlTypes.SMALLINT)
     private BeerStyle beerStyle;
 
     @NotBlank
