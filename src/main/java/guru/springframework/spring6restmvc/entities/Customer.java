181d6b58540a17bd30cc719d859380a4be3ac9af
Amonizz <andrea.monizza@gmail.com>
2025-04-29 17:47:21 +0200

    Adding new classes and relationships, adding a little test for the new repo

--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Builder
@@ -32,6 +33,8 @@ public class Customer {
     private String customerName;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
+
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
 }
-
-
